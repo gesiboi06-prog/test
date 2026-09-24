ea61259db96140ff06e336764678805de4ca372b
gesiboi06-prog <gesiboi06@gmail.com>
2026-09-24 15:03:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ playerImage.src = "image/pipo-nekonin022.png";
 
 let playerX = 100;
 let playerY = 150;
-let playerFrame = 2;
+let playerFrame = 0;
 
 
 
@@ -89,7 +89,7 @@ function updateGame() {
     if (moveDown) {
         playerY = playerY + 5;
     }
-
+playerFrame = 1;
 }
 
 
